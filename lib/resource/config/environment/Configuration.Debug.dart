fb4b8e2794c74f28e1f5f71557c9e295dd4e9526
BBSW-CuongNM <93750409+BBSW-CuongNM@users.noreply.github.com>
2022-10-25 14:55:41 +0700

    update

--- lib/resource/config/environment/Configuration.Debug.dart
+++ lib/resource/config/environment/Configuration.Debug.dart
@@ -1,12 +1,12 @@
 import '../../../shared/constants/common.dart';
 
 final Map<String, String> debugAppSettings = {
-  GraphQLAuthUrl: 'https://hico.co.jp/api',
-  GraphQLServiceUrl: 'https://hico.co.jp/api',
+  GraphQLAuthUrl: 'https://hico.dev.bluebolt.software/api',
+  GraphQLServiceUrl: 'https://hico.dev.bluebolt.software/api',
   WebSocketGraphQLUrl: 'wss://hasura.io/learn/graphql',
   MediaApiEnvironment: 'DEV',
   DefaultLanguageCode: 'vi-VN',
-  UIAPIDomain: 'https://hico.co.jp/api',
+  UIAPIDomain: 'https://hico.dev.bluebolt.software/api',
   LineChannelId: '1656904339',
   StripePublishableKey:
       'pk_live_51Gt542F8AuNSUurPdhLJmDtUprwHaDjLlntokyK6GorPlKECgV90UEdFXeflWX1Q7bimvU9H18Scb9LzBFGoz5RX00BG88HNp8',
