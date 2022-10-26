28ecc045d580ea5821258432d50a922ff1905b28
Vũ Tiến Hai <hai2662@gmail.com>
2022-10-26 09:13:58 +0700

    Update

--- lib/resource/config/environment/Configuration.Debug.dart
+++ lib/resource/config/environment/Configuration.Debug.dart
@@ -7,8 +7,16 @@ final Map<String, String> debugAppSettings = {
   MediaApiEnvironment: 'PROD',
   DefaultLanguageCode: 'vi-VN',
   UIAPIDomain: 'https://hico.dev.bluebolt.software/api',
-
   LineChannelId: '1656904339',
   StripePublishableKey:
       'pk_test_51KXiEpLepUHLp8PmntSg4hW8F1GUz8me3Sz3x7VX4wt4WleJhBmSDfqO3JTYhP1mfjKEKsWJ97axUVx2sAW77q9J006Y4kM1D3',
+  // GraphQLAuthUrl: 'https://hico.co.jp/api/',
+  // GraphQLServiceUrl: 'https://hico.co.jp/api/',
+  // WebSocketGraphQLUrl: 'wss://hasura.io/learn/graphql',
+  // MediaApiEnvironment: 'PROD',
+  // DefaultLanguageCode: 'vi-VN',
+  // UIAPIDomain: 'https://hico.co.jp/api/',
+  // LineChannelId: '1656904339',
+  // StripePublishableKey:
+  //     'pk_live_51Gt542F8AuNSUurPdhLJmDtUprwHaDjLlntokyK6GorPlKECgV90UEdFXeflWX1Q7bimvU9H18Scb9LzBFGoz5RX00BG88HNp8',
 };
--- lib/resource/config/environment/Configuration.PROD.dart
+++ lib/resource/config/environment/Configuration.PROD.dart
@@ -1,13 +1,13 @@
 import '../../../shared/constants/common.dart';
 
 final Map<String, String> productAppSettings = {
-  GraphQLAuthUrl: 'https://hico.dev.bluebolt.software/api',
-  GraphQLServiceUrl: 'https://hico.dev.bluebolt.software/api',
+  GraphQLAuthUrl: 'https://hico.co.jp/api/',
+  GraphQLServiceUrl: 'https://hico.co.jp/api/',
   WebSocketGraphQLUrl: 'wss://hasura.io/learn/graphql',
   MediaApiEnvironment: 'PROD',
   DefaultLanguageCode: 'vi-VN',
-  UIAPIDomain: 'https://hico.dev.bluebolt.software/api',
-  LineChannelId: '1656878196',
+  UIAPIDomain: 'https://hico.co.jp/api/',
+  LineChannelId: '1656904339',
   StripePublishableKey:
       'pk_live_51Gt542F8AuNSUurPdhLJmDtUprwHaDjLlntokyK6GorPlKECgV90UEdFXeflWX1Q7bimvU9H18Scb9LzBFGoz5RX00BG88HNp8',
 };
